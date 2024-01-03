854bd6421269828220e4ff03d44ce5c00f00d88a
Ally <allyy.noel@gmail.com>
2024-01-02 19:42:03 -0500

    aboutcontent

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-// Afunction toggleDropdown() {
-    function toggleDropdown() {
-        var content = document.getElementById("internshipDetails");
-        if (content.style.display === "block") {
-          content.style.display = "none";
-        } else {
-          content.style.display = "block";
-        }
-      }      
+
